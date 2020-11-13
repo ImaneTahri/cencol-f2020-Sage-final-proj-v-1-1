f47fed3002a4f7007133fbbc7efc92e49c0caefe
Imane Tahri <itahri@my.centennialcollege.ca>
2020-11-13 11:48:22 -0500

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,14 @@
+/*--
+Author: Christine Kweri
+Student ID : 300957096
+Date: 10-11-2020
+FileName : db.js
+
+Where to connect from
+*/
+
+module.exports =
+{
+   //"URI": "mongodb://localhost/contact"
+   "URI" : "mongodb+srv://imane:IPUgArLzsdGO2HXa@personalsite.jkrpw.mongodb.net/personal_site?retryWrites=true&w=majority"
+}
